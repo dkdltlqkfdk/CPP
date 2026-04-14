f118e300d79ef4a46c423eaaaaa0407c8f7b95f3
dkdltlqkfdlk <yyelin718@gmail.com>
2026-04-14 14:03:35 +0900

    p301_1

--- Week7/p301_1/1.hpp
+++ Week7/p301_1/1.hpp
@@ -1,11 +1,34 @@
 #include <iostream>
+#include <iomanip> // setw, setfill
 class MyTime {
     private:
         int hour, min, sec, msec;
 
     public:
         void convert(double duration);
-        void print();
+        void print();                        
         MyTime add(MyTime t2);
         void reset();
+        void read();
 };
+
+/* (2).
++------------------------------------------+
+|                 MyTime                   |  <-- 클래스 이름
++------------------------------------------+
+| - hour : int                             |  <-- 멤버 변수 (Attributes)
+| - min  : int                             |      ('-'는 private을 의미)
+| - sec  : int                             |
+| - msec : int                             |
++------------------------------------------+
+| + convert(duration : double) : void      |  <-- 멤버 함수 (Operations)
+| + print() : void                         |      ('+'는 public을 의미)
+| + add(t2 : MyTime) : MyTime              |
+| + reset() : void                         |
++------------------------------------------+
+
+멤버 함수는 클래스 안에 이미 변수들이 들어있기에 덜 가져와도 되고 결과물을 객체 통째로 넘겨줄 수 있어서 사용. */
+
+
+
+//(5). 헤더파일 구현
--- Week7/p301_1/2.cpp
+++ Week7/p301_1/2.cpp
@@ -9,9 +9,11 @@ inline void MyTime::convert(double duration){
     msec=(duration-d)*1000;
 }
             
-inline void MyTime::print(){
-    std::cout<<hour<<":"<<min<<":"<<sec<<":"<<msec;
-}
+inline void MyTime::print(){   
+    /*std::cout<<hour<<":"<<min<<":"<<sec<<":"<<msec;*/       //(1) 표준 출력 객체 cout를 사용해  구현하기 전 멤버 함수 print()
+    std::cout << std::setfill('0') << std::setw(2) << hour << ":" << std::setw(2) << min  << ":" << std::setw(2) << sec  << ":" << std::setw(3) << msec;
+}                                                             //(3)
+
 inline MyTime MyTime::add(MyTime t2){
     MyTime t3;
     t3.hour=hour+t2.hour;
@@ -20,15 +22,15 @@ inline MyTime MyTime::add(MyTime t2){
     t3.msec=msec+t2.msec;
 
     if (t3.msec >= 1000) {
-        t3.sec += (t3.msec / 1000); // 1000ms당 1초 올림
-        t3.msec %= 1000;            // 남은 밀리초 저장
+        t3.sec += (t3.msec / 1000); 
+        t3.msec %= 1000;           
     }
     if (t3.sec >= 60) {
-        t3.min += (t3.sec / 60);    // 60초당 1분 올림
+        t3.min += (t3.sec / 60); 
         t3.sec %= 60;
     }
     if (t3.min >= 60) {
-        t3.hour += (t3.min / 60);   // 60분당 1시간 올림
+        t3.hour += (t3.min / 60);   
         t3.min %= 60;
     }
 
@@ -39,20 +41,21 @@ inline void MyTime::reset() {
     hour = min = sec = msec = 0; 
 }
 
+inline void MyTime::read() {                                     //(4)
+    double duration;
+    std::cout<<"초 단위의 실수형 시간을 입력하시오: ";
+    std::cin>>duration;
+    convert(duration);
+}
 
 int main(){
     MyTime t1,t2,t3;
-    double duration,d2;
 
-    std::cout<<"초 단위의 실수형 시간을 입력하시오: ";
-    std::cin>>duration;
-    t1.convert(duration);
+    t1.read();
     t1.print();
     std::cout<<"\n\n";
 
-    std::cout<<"더하고 싶은 초 단위 실수형으로 입력하시오: ";
-    std::cin>>d2;
-    t2.convert(d2);
+    t2.read();
     std::cout<<"\n\n";
 
     t3=t1.add(t2);
@@ -63,3 +66,5 @@ int main(){
     t1.reset();t2.reset();t3.reset();
     return 0;
 }
+
+//(5). 소스파일 구현
